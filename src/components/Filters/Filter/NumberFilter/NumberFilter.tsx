78603c1b7fb8de20de88f1bfa3973993adad8953
Simon Mary <sma@marketlogicsoftware.com>
2022-03-06 20:34:05 +0100

    filter label

--- src/components/Filters/Filter/NumberFilter/NumberFilter.tsx
+++ src/components/Filters/Filter/NumberFilter/NumberFilter.tsx
@@ -19,24 +19,30 @@ const NumberFilter = ({parameter, title, values, addFilter}: FilterProps) => {
     <div className="number-filter-root">
       <label>{title}</label>
       <div className="number-filter-inputs">
-        <input
-          type="number"
-          name={'min'}
-          id={`${title}-min`}
-          onChange={e => setMin(e.target.value)}
-          min={values[0]}
-          max={values[values.length - 1]}
-          value={min}
-        />
-        <input
-          type="number"
-          name={'max'}
-          id={`${title}-max`}
-          onChange={e => setMax(e.target.value)}
-          min={values[0]}
-          max={values[values.length - 1]}
-          value={max}
-        />
+        <div className="number-filter-selector-root">
+          <span>from:</span>
+          <input
+            type="number"
+            name={'min'}
+            id={`${title}-min`}
+            onChange={e => setMin(e.target.value)}
+            min={values[0]}
+            max={values[values.length - 1]}
+            value={min}
+          />
+        </div>
+        <div className="number-filter-selector-root">
+          <span>to:</span>
+          <input
+            type="number"
+            name={'max'}
+            id={`${title}-max`}
+            onChange={e => setMax(e.target.value)}
+            min={values[0]}
+            max={values[values.length - 1]}
+            value={max}
+          />
+        </div>
       </div>
     </div>
   );
